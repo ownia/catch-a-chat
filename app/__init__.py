08b4b8117a6608b26766073994d28bac5def7669
ownia <451686892@qq.com>
2019-05-31 21:06:32 +0800

    flask DEBUG error

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,7 +16,34 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-        auth = (app.config['MAIL_USERNAME'],app.config)
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        # 创建一个SMTPHandler实例报告错误日志并将其附到pp.logger对象上
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='CatchAChat Failure',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
-from app import routes, models, errors
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    # RotatingFileHandler类可以确保日志文件不会因为长时间运行应用变的特别大
+    file_handler = RotatingFileHandler('logs/CatchAChat.log', maxBytes=10240, backupCount=10)
+    # logging.Formatter提供了日志消息的自定义格式
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('CatchAChat startup')
+
+    from app import routes, models, errors
