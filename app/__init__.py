d3c46878d8b10ba7575ca498e6f125aa944db8e0
ownia <451686892@qq.com>
2019-06-01 21:52:44 +0800

    change to Flask-Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,11 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -17,6 +19,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
