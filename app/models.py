81c15b2c833af4f8e703e986512fb20936a8cb49
ownia <451686892@qq.com>
2019-06-02 19:26:37 +0800

    reset the database sadly

--- app/models.py
+++ app/models.py
@@ -81,9 +81,10 @@ def load_user(id):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(64))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Pos {}>'.format(self.body)
