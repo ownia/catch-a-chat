a4a0c9754b363b059cec3fd6b2707e2641bac096
ownia <451686892@qq.com>
2019-06-04 14:12:09 +0800

    init branch heroku

--- config.py
+++ config.py
@@ -28,3 +28,5 @@ class Config(object):
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     # 配置Redis
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    #
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
