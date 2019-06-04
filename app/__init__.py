f7f306948b31f855b48bd64b8275414827aaa1fd
ownia <451686892@qq.com>
2019-06-04 14:21:15 +0800

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -76,15 +76,18 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        # RotatingFileHandler类可以确保日志文件不会因为长时间运行应用变的特别大
-        file_handler = RotatingFileHandler('logs/ChatAChat.log', maxBytes=10240, backupCount=10)
-        # logging.Formatter提供了日志消息的自定义格式
-        file_handler.setFormatter(
-            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/CatchAChat.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(
+                logging.Formatter('%(asctime)s %(levelname)s: %(message)s ' '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('CatchAChat startup')
