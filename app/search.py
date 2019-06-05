356e35cb9707c058169b29f65439addd0203b9a7
ownia <451686892@qq.com>
2019-06-05 10:29:05 +0800

    update elasticsearch finally

--- app/search.py
+++ app/search.py
@@ -20,8 +20,8 @@ def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
-        index=index, doc_type=index,
+        index=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}}, 'from': (page - 1) * per_page,
               'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
-    return ids, search['hits']['total']
+    return ids, search['hits']['total']['value']
